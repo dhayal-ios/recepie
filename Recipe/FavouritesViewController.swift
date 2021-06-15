c6f95cb9a688b1e088f8c608ddf146352bffd34f
sd <dhayal.cluzter@gmail.com>
2021-06-15 13:29:37 +0530

    Tabbed app

---
+++ Recipe/FavouritesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavouritesViewController.swift
+//  Recipe
+//
+//  Created by dhayalprema on 15/06/21.
+//  Copyright © 2021 dhayalprema. All rights reserved.
+//
+
+import UIKit
+
+class FavouritesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
